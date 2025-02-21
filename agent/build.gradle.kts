5d6623433ea0e7bf785dbe6070818d2f834121e8
Sōta <sota@sota.moe>
2025-02-21 16:49:31 +0100

    chore: update agent manifest

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -6,5 +6,7 @@ group = "moe.sota"
 
 tasks.jar {
     archiveExtension = "zip"
-    manifest.attributes("Agent-Class" to "${project.group}.${rootProject.name.lowercase()}.Main")
+    manifest {
+        attributes("Agent-Class" to "${project.group}.${rootProject.name.lowercase()}.Main")
+    }
 }
