072d6ce39cea29e8b71c07ccac04cf806e75b772
Sōta <sota@sota.moe>
2025-03-10 18:00:22 +0100

    feat: add agent lombok (#14)

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -1,12 +1,22 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.12.1"
 }
 
 group = "moe.sota"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
 tasks.jar {
     archiveExtension = "zip"
     manifest {
-        attributes("Agent-Class" to "${project.group}.${rootProject.name.lowercase()}.Main")
+        attributes("Agent-Class" to "${project.group}.${rootProject.name.lowercase()}.${project.name}.Main")
     }
 }
--- agent/src/main/java/moe/sota/decompiler/agent/Main.java
+++ agent/src/main/java/moe/sota/decompiler/agent/Main.java
@@ -1,7 +1,10 @@
 package moe.sota.decompiler.agent;
 
+import lombok.experimental.UtilityClass;
+
 import java.lang.instrument.Instrumentation;
 
+@UtilityClass
 public class Main {
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
